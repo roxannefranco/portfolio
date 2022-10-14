748b17a8da0e2709bf7e16039259b95c8cc97028
Roxanne Franco <roxannefranco10@gmail.com>
2022-10-14 20:49:58 +0200

    importing array of projects into website

--- js/projectsList.js
+++ js/projectsList.js
@@ -4,6 +4,8 @@ export const projects = [
     description:
       "An online store for a Rainjacket’s brand aimed to be affordable, durable and reliable to every outdoor enthusiast! Developed as my first project at my Front-end Development studies at Noroff, this online store is meant to have a simple, clear and easy shopping flow for a better user experience.",
     image: "images/rainydays-screens.svg",
+    link: "https://rainydayswear.netlify.app/",
+    github: "https://github.com/roxannefranco/RainyDays",
     id: "1",
   },
   {
@@ -11,6 +13,8 @@ export const projects = [
     description:
       "A Science Museum recently opened with a diverse range of interactive exhibits for a core target audience that is inquisitive and intelligent primary and middle school children (ages between 7-15) and families with young children. The website is aimed to be informative and appealing. All exhibits were a criation for this project. Developed as my Semester Project as a student of Front-end Development at Noroff, this online store is meant to inform and appeal its users into booking tickets for the museum. It has a simple, clear and easy flow for a better user experience. At this point we were not yet using Javascript at the course.",
     image: "images/museum-screens.svg",
+    link: "https://project-community-science-museum.netlify.app/",
+    github: "https://github.com/roxannefranco/community-science-museum",
     id: "2",
   },
   {
@@ -18,6 +22,8 @@ export const projects = [
     description:
       "MyBergen is a travel guide Blog with a Headless WordPress REST API to manage the website's posts. Developed as my 1st year Exam Project for Front-end Development at Noroff, there were the mandatory aspects to include in the project. (listed in my Github!)",
     image: "images/mybergen-screens.svg",
+    link: "https://mybergen.netlify.app/",
+    github: "https://github.com/roxannefranco/mybergen/blob/main/README.md",
     id: "3",
   },
 ];
